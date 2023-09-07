8a37800d1c1044b763a894c522a9b620a2a717d4
thiago-weslley <thiagoweslley@yahoo.com>
2023-09-07 17:32:02 -0300

    feat: Aplica tema para aplicacão com `theme.js`

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
+
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+
+import { Details } from "./pages/Detais";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    background_800: "#232129",
+    background_700: "#312E38",
+    background_600: "#3E3B47",
+
+    gray_100: "#A9A9A9",
+    gray_200: "#666360",
+
+    white_100: "#FFFAF0",
+
+    orange: "#FF9000",
+
+    red: "#DC143C",
+  },
+};
